cde6baa0f30c4ed7c79310cf257d59ca02f90c5d
Jesse Andrews <jesse@planet.com>
2019-11-17 04:19:10 -0800

    pouchdb added

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import PouchDB from "pouchdb-browser";
+  export let name;
+  let db = new PouchDB("test");
+  console.log(db);
+</script>
+
+<style>
+
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+</main>
